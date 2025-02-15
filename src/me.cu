223927ec3c32664cb3e0f4afb1f4e81c7f9ba296
elstr-512 <122643627+elstr-512@users.noreply.github.com>
2025-02-15 16:55:23 +0100

    cleanup

--- src/me.cu
+++ src/me.cu
@@ -181,205 +181,39 @@ void c63_motion_compensate(struct c63_common *cm)
 /*                  CUDA SECTION                      */
 /* ~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~ */
 
-__device__ static void gpu_sad_block_8x8(uint8_t *block1, uint8_t *block2, int stride, int *result)
+static void gpu_sad_block_8x8()
 {
-  int u, v;
 
-  *result = 0;
-
-  for (v = 0; v < 8; ++v)
-  {
-    for (u = 0; u < 8; ++u)
-    {
-      *result += abs(block2[v*stride+u] - block1[v*stride+u]);
-    }
-  }
 }
 
 /* Motion estimation for 8x8 block */
-__device__ static void gpu_me_block_8x8(struct c63_common *cm, int mb_x, int mb_y,
-    uint8_t *orig, uint8_t *ref, int color_component)
+static void gpu_me_block_8x8()
 {
-  struct macroblock *mb =
-    &cm->curframe->mbs[color_component][mb_y*cm->padw[color_component]/8+mb_x];
 
-  int range = cm->me_search_range;
-
-  /* Quarter resolution for chroma channels. */
-  if (color_component > 0) { range /= 2; }
-
-  int left = mb_x * 8 - range;
-  int top = mb_y * 8 - range;
-  int right = mb_x * 8 + range;
-  int bottom = mb_y * 8 + range;
-
-  int w = cm->padw[color_component];
-  int h = cm->padh[color_component];
-
-  /* Make sure we are within bounds of reference frame. TODO: Support partial
-     frame bounds. */
-  if (left < 0) { left = 0; }
-  if (top < 0) { top = 0; }
-  if (right > (w - 8)) { right = w - 8; }
-  if (bottom > (h - 8)) { bottom = h - 8; }
-
-  int x, y;
-
-  int mx = mb_x * 8;
-  int my = mb_y * 8;
-
-  int best_sad = INT_MAX;
-
-  for (y = top; y < bottom; ++y)
-  {
-    for (x = left; x < right; ++x)
-    {
-      int sad;
-      gpu_sad_block_8x8(orig + my*w+mx, ref + y*w+x, w, &sad);
-
-      /* printf("(%4d,%4d) - %d\n", x, y, sad); */
-
-      if (sad < best_sad)
-      {
-        mb->mv_x = x - mx;
-        mb->mv_y = y - my;
-        best_sad = sad;
-      }
-    }
-  }
-
-  /* Here, there should be a threshold on SAD that checks if the motion vector
-     is cheaper than intraprediction. We always assume MV to be beneficial */
-
-  /* printf("Using motion vector (%d, %d) with SAD %d\n", mb->mv_x, mb->mv_y,
-     best_sad); */
-
-  mb->use_mv = 1;
 }
 
-__global__ void gpu_c63_motion_estimate_kernel(struct c63_common *cm)
+__global__ static void gpu_c63_motion_estimate_kernel()
 {
-  /* Compare this frame with previous reconstructed frame */
-  int mb_x, mb_y;
 
-  /* Luma */
-  for (mb_y = 0; mb_y < cm->mb_rows; ++mb_y)
-  {
-    for (mb_x = 0; mb_x < cm->mb_cols; ++mb_x)
-    {
-      gpu_me_block_8x8(cm, mb_x, mb_y, cm->curframe->orig->Y,
-          cm->refframe->recons->Y, Y_COMPONENT);
-    }
-  }
-
-  /* Chroma */
-  for (mb_y = 0; mb_y < cm->mb_rows / 2; ++mb_y)
-  {
-    for (mb_x = 0; mb_x < cm->mb_cols / 2; ++mb_x)
-    {
-      gpu_me_block_8x8(cm, mb_x, mb_y, cm->curframe->orig->U,
-          cm->refframe->recons->U, U_COMPONENT);
-      gpu_me_block_8x8(cm, mb_x, mb_y, cm->curframe->orig->V,
-          cm->refframe->recons->V, V_COMPONENT);
-    }
-  }
 }
 
 void gpu_c63_motion_estimate(struct c63_common *cm)
 {
-  int width = cm->width;
-  int height = cm->height;
 
-  int num_blocks_x = width / cm->mb_cols;
-  int num_blocks_y = height / cm->mb_rows;
-
-  // This should hold
-  // assert(num_blocks_x == 8);
-  // assert(num_blocks_y == 8);
-
-  c63_common *d_cm;
-  cudaMalloc(&d_cm, sizeof(c63_common));
-  cudaMemcpy(d_cm, cm, sizeof(c63_common), cudaMemcpyHostToDevice);
-  
-
-  dim3 blockDim(num_blocks_x, num_blocks_y);
-  dim3 gridDim(num_blocks_x / blockDim.x, num_blocks_y / blockDim.y);
-  gpu_c63_motion_estimate_kernel<<<gridDim, blockDim>>>(cm);
 }
 
 /* Motion compensation for 8x8 block */
-__device__ void gpu_mc_block_8x8(struct c63_common *cm, int mb_x, int mb_y,
-    uint8_t *predicted, uint8_t *ref, int color_component)
+static void gpu_mc_block_8x8()
 {
-  struct macroblock *mb =
-    &cm->curframe->mbs[color_component][mb_y*cm->padw[color_component]/8+mb_x];
 
-  if (!mb->use_mv) { return; }
-
-  int left = mb_x * 8;
-  int top = mb_y * 8;
-  int right = left + 8;
-  int bottom = top + 8;
-
-  int w = cm->padw[color_component];
-
-  /* Copy block from ref mandated by MV */
-  int x, y;
-
-  for (y = top; y < bottom; ++y)
-  {
-    for (x = left; x < right; ++x)
-    {
-      predicted[y*w+x] = ref[(y + mb->mv_y) * w + (x + mb->mv_x)];
-    }
-  }
 }
 
-__global__ void gpu_c63_motion_compensate_kernel(struct c63_common *cm)
+__global__ static void gpu_c63_motion_compensate_kernel()
 {
-  int mb_x, mb_y;
 
-  /* Luma */
-  for (mb_y = 0; mb_y < cm->mb_rows; ++mb_y)
-  {
-    for (mb_x = 0; mb_x < cm->mb_cols; ++mb_x)
-    {
-      gpu_mc_block_8x8(cm, mb_x, mb_y, cm->curframe->predicted->Y,
-          cm->refframe->recons->Y, Y_COMPONENT);
-    }
-  }
-
-  /* Chroma */
-  for (mb_y = 0; mb_y < cm->mb_rows / 2; ++mb_y)
-  {
-    for (mb_x = 0; mb_x < cm->mb_cols / 2; ++mb_x)
-    {
-      gpu_mc_block_8x8(cm, mb_x, mb_y, cm->curframe->predicted->U,
-          cm->refframe->recons->U, U_COMPONENT);
-      gpu_mc_block_8x8(cm, mb_x, mb_y, cm->curframe->predicted->V,
-          cm->refframe->recons->V, V_COMPONENT);
-    }
-  }
 }
 
 void gpu_c63_motion_compensate(struct c63_common *cm)
 {
-  int width = cm->width;
-  int height = cm->height;
 
-  int num_blocks_x = width / cm->mb_cols;
-  int num_blocks_y = height / cm->mb_rows;
-
-  // This should hold
-  // assert(num_blocks_x == 8);
-  // assert(num_blocks_y == 8);
-
-  c63_common *d_cm;
-  cudaMalloc(&d_cm, sizeof(c63_common));
-  cudaMemcpy(d_cm, cm, sizeof(c63_common), cudaMemcpyHostToDevice);
-  
-
-  dim3 blockDim(num_blocks_x, num_blocks_y);
-  dim3 gridDim(num_blocks_x / blockDim.x, num_blocks_y / blockDim.y);
-  gpu_c63_motion_compensate_kernel<<<gridDim, blockDim>>>(cm);
 } 
